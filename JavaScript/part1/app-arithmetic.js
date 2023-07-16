867d542d19327486731a1680f537b3f8385a5567
Shivam Verma <98605616+sv959@users.noreply.github.com>
2023-07-16 15:50:36 +0530

    .

---
+++ JavaScript/part1/app-arithmetic.js
@@ -0,0 +1,31 @@
+
+
+// // #arithmetic operators in javascript
+
+let a = 15;
+let b = 25;
+console.log(a++)    //15
+console.log(++a)    //17
+
+let pencil = 10;
+let eraserPrice = 20;
+
+// // let bill =
+// //     "Thankyou sir! Your bill is *" + (pencil + eraserPrice) + "* Rupees. Visit us again?"
+
+let bill =
+    `Thankyou sir! Your bill is *${pencil + eraserPrice}Rupees. Visit us again?`
+
+console.log(bill)
+
+
+// // console.log("hello shivam and i am owner of this website")
+// console.log("you can maybe call me author of this website as well.")
+
+// let a=15;
+// let b=35;
+
+// let mean = (a + b)/2
+// console.log(mean)
+
+// // this is a Comment
